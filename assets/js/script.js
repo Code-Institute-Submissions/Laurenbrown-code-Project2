103b465b5a71af15299c3a4d9ed38e4a1fecdd4e
Laurenbrown-code <lbrown1443@gmail.com>
2021-10-02 20:23:12 +0000

    add answers to questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const question = document.querySelector('#question');
 const answers = Array.from(document.querySelectorAll('.answer-text'));
-const progressText = document.querySelector('#progressText');
+const progress = document.querySelector('#progress');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
@@ -13,73 +13,73 @@ let availableQuestion = []
 let questions = [
 {
     question: "Which game involving fruit is commonly played at Halloween?",
-    optionA:"Bobbing for apples",
-    optionB:"Juggling bananas",
-    optionC:"Smashing pineapples",
-    correctAnswer:"A",
+    option1:"Bobbing for apples",
+    option2:"Juggling bananas",
+    option3:"Smashing pineapples",
+    correctAnswer:"1",
 },
 {
     question: "Who are Dr. Peter Venkman, Dr. Raymond Stantz, and Dr. Egon Spengler better known as?",
-    optionA:"The Ghostbusters",
-    optionB:"Five Guys",
-    optionC:"The A Team",
-    correctAnswer:"A",
+    option1:"The Ghostbusters",
+    option2:"Five Guys",
+    option3:"The A Team",
+    correctAnswer:"1",
 },
 {
     question: "Who sang the 1962 song 'Monster Mash'?",
-    optionA:"The Clash",
-    optionB:"Bobby Pickett and the Crypt Kickers",
-    optionC:"Creepers",
-    correctAnswer:"B",
+    option1:"The Clash",
+    option2:"Bobby Pickett and the Crypt Kickers",
+    option3:"Creepers",
+    correctAnswer:"2",
 },
 {
     question:"On Halloween, if a black cat crosses your path, what will you receive?",
-    optionA:"£200",
-    optionB:"A dodgy stomach",
-    optionC:"Bad Luck",
-    correctAnswer:"C",
+    option1:"£200",
+    option2:"A dodgy stomach",
+    option3:"Bad Luck",
+    correctAnswer:"3",
 },
 {
     question: "The word 'witch' comes from an old English word meaning...",
-    optionA:"Wise Woman",
-    optionB:"Scary Features",
-    optionC:"Pointy Nose",
-    correctAnswer:"A",
+    option1:"Wise Woman",
+    option2:"Scary Features",
+    option3:"Pointy Nose",
+    correctAnswer:"1",
 },
 {
     question: "What did the Scottish Witchcraft Act of 1735 forbid the consumption of?",
-    optionA:"Pumpkins",
-    optionB:"Sausage Rolls",
-    optionC:"Treacle Toffee",
-    correctAnswer:"B",
+    option1:"Pumpkins",
+    option2:"Sausage Rolls",
+    option3:"Treacle Toffee",
+    correctAnswer:"2",
 },
 {
     question:"What type of vegetable is disliked by vampires and is used to frighten them away?",
-    optionA:"Onions",
-    optionB:"Garlic",
-    optionC:"Parsnips",
-    correctAnswer:"B",
+    option1:"Onions",
+    option2:"Garlic",
+    option3:"Parsnips",
+    correctAnswer:"2",
 },
 {
     question:"True or false: The word ‘hallow’ means saint or holy person?",
-    optionA:"True",
-    optionB:"False",
-    optionC:"Don't Know",
-    correctAnswer:"A",
+    option1:"True",
+    option2:"False",
+    option3:"Don't Know",
+    correctAnswer:"1",
 },
 {
     question:"Which magical characters must be appeased with offerings of biscuits and a saucer of milk?",
-    optionA:"House elves",
-    optionB:"Pixies",
-    optionC:"The Borrowers",
-    correctAnswer:"A",
+    option1:"House elves",
+    option2:"Pixies",
+    option3:"The Borrowers",
+    correctAnswer:"1",
 },
 {
     question:"What is the day after Hallowe’en called?",
-    optionA:"Monday",
-    optionB:"All Souls Day",
-    optionC:"Treat Day",
-    correctAnswer:"B",
+    option1:"Monday",
+    option2:"All Souls Day",
+    option3:"Treat Day",
+    correctAnswer:"2",
 },
 ]
 
@@ -101,31 +101,31 @@ getNewQuestion = () => {
     }
 
     questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progress.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
     progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestion.length)
     currentQuestion = availableQuestion[questionsIndex]
     question.innerText = currentQuestion.question
 
-    option.forEach(option => {
-        const number = option.dataset['number']
-        option.innerText = currentQuestion['option' + number]
+    answers.forEach(answers => {
+        const number = answers.dataset['number']
+        answers.innerText = currentQuestion['option' + number]
     })
 
     availableQuestion.splice(questionsIndex, 1)
     acceptingAnswers = true
 }
 
-option.forEach(option => {
-    option.addEventListener('click', e => {
+answers.forEach(answers => {
+    answers.addEventListener('click', e => {
         if(!acceptingAnswers) return
 
         acceptingAnswers = false
         const selectedOption = e.target
         const selectedAnswer = selectedOption.dataset['number']
 
-        let classToApply = selectedAnswer == currentQuestion.correctAnswer ? 'correct' : 'incorrect'
+        let classToApply = selectedAnswer == currentQuestion.answers ? 'correct' : 'incorrect'
 
         if (classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
@@ -149,3 +149,4 @@ incrementScore = num => {
 }
 
 startGame()
+
